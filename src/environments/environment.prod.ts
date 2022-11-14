6dbbe46a2c911a7386228d91ac2a0a2943619fbe
Boby <44772249+bori0409@users.noreply.github.com>
2022-11-14 16:48:51 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  firebaseConfig:{}
+  firebaseConfig:{
+    
+      apiKey: "AIzaSyCGEoJURKc7mhW7M-iPwSWlvWhlofB7Pcg",
+      authDomain: "fridge-app-cool.firebaseapp.com",
+      projectId: "fridge-app-cool",
+      storageBucket: "fridge-app-cool.appspot.com",
+      messagingSenderId: "230543031921",
+      appId: "1:230543031921:web:3cb83d69bed2c35e7d65d0"
+    
+  }
 };
