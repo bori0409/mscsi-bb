196f2ae0b92d346f41bc793a8550b1775a713ffc
Boby <44772249+bori0409@users.noreply.github.com>
2023-05-04 10:53:48 +0200

    try webkit

--- src/app/components/try1/try1.component.ts
+++ src/app/components/try1/try1.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { log } from 'console';
-
+ declare const window: any;
 @Component({
   selector: 'app-try1',
   templateUrl: './try1.component.html',
   styleUrls: ['./try1.component.css']
 })
 export class Try1Component implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit(): void {
@@ -18,7 +18,18 @@ export class Try1Component implements OnInit {
       console.log("Vibration supported on this device");
     } else {
       console.log("Vibration not supported on this device");
-    }
-
+    }    
 }
+
+vibrate2() {
+  if ("vibrate" in navigator) {
+    navigator.vibrate(500);
+  } else if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.vibrate) {
+    window.webkit.messageHandlers.vibrate.postMessage({});
+  } else {
+    console.log("Vibration not supported on this device");
+  }
+}
+
+
 }
