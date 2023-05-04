44ea38f8dbd618fa5ce7d17f059424dbd44ad32e
Boby <44772249+bori0409@users.noreply.github.com>
2023-05-04 11:49:58 +0200

    try noe

--- src/app/components/try1/try1.component.ts
+++ src/app/components/try1/try1.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { log } from 'console';
 import { VibrationService } from 'src/app/shared/vibration.service';
+
  declare const window: any;
 @Component({
   selector: 'app-try1',
@@ -8,7 +9,8 @@ import { VibrationService } from 'src/app/shared/vibration.service';
   styleUrls: ['./try1.component.css']
 })
 export class Try1Component implements OnInit {
- 
+ howManyTimes = 0;
+ mess = "hello";
   constructor(private vibrationService: VibrationService) { }
 
   ngOnInit(): void {
@@ -18,8 +20,11 @@ export class Try1Component implements OnInit {
     if ("vibrate" in navigator) {
       navigator.vibrate(500);
       console.log("Vibration supported on this device");
+      this.howManyTimes++;
+      this.mess = "Vibration supported on this device, number 1";
     } else {
       console.log("Vibration not supported on this device");
+      this.mess = "Vibration NOT supported on this device, number 1";
     }    
 }
 
@@ -28,13 +33,29 @@ vibrate2() {
     navigator.vibrate(500);
   } else if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.vibrate) {
     window.webkit.messageHandlers.vibrate.postMessage({});
+    this.howManyTimes++;
+    this.mess = "Vibration supported on this device, number 2";
   } else {
     console.log("Vibration not supported on this device");
+    this.mess = "Vibration NOT supported on this device, number 1";
   }
 }
 vibrate3() {
   this.vibrationService.vibrate(1000); 
+  this.howManyTimes++;
+  this.mess = "Vibration supported on this device, number 3";
+
   
+}
+vibrate4() {
+  if (window.navigator && window.navigator.vibrate) {
+    navigator.vibrate(1000);
+    // Vibration supported
+    this.mess = "Vibration NOT supported on this device, number 4";
+ } else {
+    // Vibration not supported
+    this.mess = "Vibration NOT supported on this device, number 4";
+ }
 }
 
 }
